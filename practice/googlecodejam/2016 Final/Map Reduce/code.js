5587e56307e534b62d4a0d7a1760dbd2e98ab260
Jack Le Hamster <jacklehamster@gmail.com>
2017-04-12 00:24:22 -0700

    test

---
+++ practice/googlecodejam/2016 Final/Map Reduce/code.js
@@ -0,0 +1,59 @@
+var reader = new FileReader();
+
+function readText(that){
+
+    if(that.files && that.files[0]){
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var output=e.target.result;
+            process(output);
+        };
+        reader.readAsText(that.files[0]);
+    }
+}
+
+
+function LoadFile() {
+    var oFrame = document.getElementById("frmFile");
+    var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
+    while (strRawContents.indexOf("\r") >= 0)
+        strRawContents = strRawContents.replace("\r", "");
+    process(strRawContents);
+}
+
+function StreamReader(stream) {
+    this.stream = stream;
+};
+StreamReader.prototype.readString = function() {
+    var split = this.stream.split(" ");
+    var first = split[0];
+    this.stream = split.slice(1).join(" ");
+    return first;
+};
+StreamReader.prototype.readChar = function() {
+    var ch = this.stream.charAt(0);
+    this.stream = this.stream.slice(1);
+    return ch;
+};
+StreamReader.prototype.readLine = function() {
+    var split = this.stream.split("\n");
+    var first = split[0];
+    this.stream = split.slice(1).join("\n");
+    return first;
+};
+
+function process(output) {
+    document.getElementById("result").innerHTML = "";
+    var stream = new StreamReader(output);
+    var count = parseInt(stream.readLine());
+
+    for(var i=0;i < count; i++) {
+        cout("Case #"+(i+1)+": ");
+        solve(stream);
+    }
+}
+
+function cout(str) {
+    console.log(str);
+    document.getElementById("result").innerHTML += str;
+}
---
+++ practice/googlecodejam/2016 Final/Map Reduce/solve.js
@@ -0,0 +1,129 @@
+function parse(stream) {
+    var R = parseInt(stream.readString());
+    var C = parseInt(stream.readString());
+    var D = parseInt(stream.readLine());
+
+    var map = [];
+    for(var y=0;y<R;y++) {
+        map.push(stream.readLine());
+    }
+    return {
+        R:R,
+        C:C,
+        D:D,
+        map:map,
+    };
+}
+
+function solve(stream) {
+    var input = parse(stream);
+    var map = input.map;
+
+    var start = {x:0,y:0};
+    var distStart = {};
+    var distFinish = {};
+    for(var y=0;y<map.length;y++) {
+        var x = map[y].indexOf("S");
+        if(x>=0) {
+            start.x = x;
+            start.y = y;
+            travel(x,y,input,0,distStart,{});
+        }
+        x = map[y].indexOf("F");
+        if(x>=0) {
+            travel(x,y,input,0,distFinish,{});
+        }
+    }
+    cout(input.D+"\n");
+
+    for(var i in distStart) {
+        for(var s in distStart[i]) {
+            for(var f in distFinish[i]) {
+                var total = parseInt(s) + parseInt(f);
+                if(total==input.D) {
+                    cout("POSSIBLE\n");
+                    var split = i.split("_");
+                    var xx = parseInt(split[0]);
+                    var yy = parseInt(split[1]);
+                    console.log(xx,yy);
+                    if(map[yy].charAt(xx)==='#') {
+                        changeMap(map,xx,yy,'X');
+                    }
+
+//            testGame(start.x,start.y,input,0,{});
+
+                    for(var y=0;y<map.length;y++) {
+                        cout(map[y]+"\n");
+                    }
+
+                    return;
+                }
+            }
+        }
+    }
+
+    cout("IMPOSSIBLE\n");
+    return;
+}
+
+function changeMap(map,x,y,value) {
+    var r = map[y].split("");
+    r.splice(x,1,value);
+    map[y] = r.join("");
+    return map;
+}
+
+function testGame(x,y,dimension,dist,visited) {
+    if(outside(x,y,dimension)) return;
+    if(visited[x+"_"+y]) return;
+    visited[x+"_"+y] = true;
+
+    if(dimension.map[y][x]==='F') {
+        console.log(dist,dimension.D);
+        if(dist===dimension.D)
+            return true;
+    } else if(dist && dimension.map[y][x]!=='.') {
+        return false;
+    }
+    if(testGame(x+1,y,dimension,dist+1,visited)) {
+        changeMap(dimension.map,x,y,dist%10);
+        return true;
+    }
+    if(testGame(x-1,y,dimension,dist+1,visited)) {
+        changeMap(dimension.map,x,y,dist%10);
+        return true;
+    }
+    if(testGame(x,y+1,dimension,dist+1,visited)) {
+        changeMap(dimension.map,x,y,dist%10);
+        return true;
+    }
+    if(testGame(x,y-1,dimension,dist+1,visited)) {
+        changeMap(dimension.map,x,y,dist%10);
+        return true;
+    }
+
+    visited[x+"_"+y] = false;
+    return false;
+}
+
+function outside(x,y,dimension) {
+    return x<0 || y<0 || x>=dimension.C || y>=dimension.R;
+}
+
+function travel(x,y,dimension,dist,distReport,visited) {
+    if(outside(x,y,dimension)) return;
+    if(visited[x+"_"+y]) return;
+    visited[x+"_"+y] = true;
+
+    if(!distReport[x+"_"+y]) {
+        distReport[x+"_"+y] = {};
+    }
+    distReport[x+"_"+y][dist] = true;
+
+    if(dimension.map[y][x]==='.' || dist===0) {
+        travel(x-1,y,dimension,dist+1,distReport,visited);
+        travel(x+1,y,dimension,dist+1,distReport,visited);
+        travel(x,y-1,dimension,dist+1,distReport,visited);
+        travel(x,y+1,dimension,dist+1,distReport,visited);
+    }
+}
